0b00f41fea6264d8173ef29853c20a5385218a75
Michael L Clark Jr <michael.clark.jr@gmail.com>
2015-04-28 20:51:30 -0400

    initial work on intent service

---
+++ app/src/main/java/nku/edu/beerbuddyandroid/BeerIntentService.java
@@ -0,0 +1,64 @@
+package nku.edu.beerbuddyandroid;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BeerIntentService extends IntentService
+{
+    private ArrayList<BeerItem> beerlist= new ArrayList<BeerItem>();
+
+    public BeerIntentService(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet("URL");
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream inputStream = entity.getContent();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                inputStream.close();
+            } else {
+                Log.d("JSON", "Failed to download file");
+            }
+        } catch (Exception e) {
+            Log.d("readJSONFeed", e.getMessage());
+        }
+
+        //return stringBuilder.toString();
+    }
+}
--- app/src/main/java/nku/edu/beerbuddyandroid/MainActivity.java
+++ app/src/main/java/nku/edu/beerbuddyandroid/MainActivity.java
@@ -16,43 +16,36 @@ import android.os.Build;
 import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity
+{
     BeerFragment beerFrag;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BeerService BS = new BeerService();
-
-
-       // FragmentManager fragMan = getFragmentManager();
-       // FragmentTransaction fragTransaction = fragMan.beginTransaction();
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-
 }
